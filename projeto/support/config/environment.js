5a65943a9215dd6e5425179af4df01efe861012c
AndressaComp <136382126+AndressaComp@users.noreply.github.com>
2023-10-02 18:15:05 -0300

    Update environment.js

--- projeto/support/config/environment.js
+++ projeto/support/config/environment.js
@@ -25,6 +25,65 @@ export const testConfig = {
                 {duration: '10s', target: 10},
                 {duration: '10s', target: 10}
             ]
+        },
+
+        volumetria100: {
+            vus: 100,
+            thresholds: {
+                http_req_duration: ['p(95)<200'],
+                http_req_failed: ['rate<0.01']
+            }
+        },
+
+        volumetria50: {
+            vus: 50,
+            thresholds: {
+                http_req_duration: ['p(95)<2000'],
+                http_req_failed: ['rate<0.01']
+            }
+        },
+
+        volumetria30: {
+            vus: 30,
+            thresholds: {
+                http_req_duration: ['p(95)<2000'],
+                http_req_failed: ['rate<0.01']
+            }
+        },
+
+        metrica400: {
+            thresholds: {
+                http_req_duration: ['p(95)<400'],
+                http_req_failed: ['rate<0.01']
+            }
+        },
+
+        metrica300: {
+            thresholds: {
+                http_req_duration: ['p(95)<300'],
+                http_req_failed: ['rate<0.01']
+            }
+        },
+
+        metrica200: {
+            thresholds: {
+                http_req_duration: ['p(95)<200'],
+                http_req_failed: ['rate<0.01']
+            }
+        },
+
+        metrica100: {
+            thresholds: {
+                http_req_duration: ['p(95)<100'],
+                http_req_failed: ['rate<0.01']
+            }
+        },
+
+        metrica50: {
+            thresholds: {
+                http_req_duration: ['p(95)<50'],
+                http_req_failed: ['rate<0.01']
+            }
         }
     }
 }
