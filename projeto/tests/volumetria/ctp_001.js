4c79d8831aa59cc13871bb09de2a8038eb179155
AndressaComp <136382126+AndressaComp@users.noreply.github.com>
2023-10-02 18:43:58 -0300

    Update ctp_001.js

--- projeto/tests/volumetria/ctp_001.js
+++ projeto/tests/volumetria/ctp_001.js
@@ -2,7 +2,7 @@ import { sleep } from 'k6';
 import { SharedArray } from 'k6/data';
 import { BaseRest, BaseChecks, ENDPOINTS, testConfig } from '../support/base/baseTest.js';
 
-export const options = testConfig.options.smokeThresholds;
+export const options = testConfig.options.volumetria100;
 
 // Criando as variaveis base
 const base_uri = testConfig.environment.hml.url;
@@ -10,6 +10,21 @@ const baseRest = new BaseRest(base_uri);
 const baseChecks = new BaseChecks();
 
 const data = new SharedArray('Movies', function () {
-    const jsonData = JSON.parse(open('../data/static/user.json'));
-    return jsonData.users;
+    const jsonData = JSON.parse(open('../data/static/movie.json'));
+    return jsonData.movies;
 });
+
+const payload = {
+  "title": "Vingadores",
+  "description": "Super herois.",
+  "launchdate": "2023-09-24",
+  "showtimes": [
+    "inicio: 22:00"
+  ]
+}
+
+
+
+
+
+
