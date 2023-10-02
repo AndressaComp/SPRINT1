0745524f80bdef3498670f89d6c3e2ed42500dc0
AndressaComp <136382126+AndressaComp@users.noreply.github.com>
2023-10-02 18:32:23 -0300

    Update ctp_001.js

--- projeto/tests/volumetria/ctp_001.js
+++ projeto/tests/volumetria/ctp_001.js
@@ -1,1 +1,15 @@
+import { sleep } from 'k6';
+import { SharedArray } from 'k6/data';
+import { BaseRest, BaseChecks, ENDPOINTS, testConfig } from '../support/base/baseTest.js';
 
+export const options = testConfig.options.smokeThresholds;
+
+// Criando as variaveis base
+const base_uri = testConfig.environment.hml.url;
+const baseRest = new BaseRest(base_uri);
+const baseChecks = new BaseChecks();
+
+const data = new SharedArray('Movies', function () {
+    const jsonData = JSON.parse(open('../data/static/user.json'));
+    return jsonData.users;
+});
